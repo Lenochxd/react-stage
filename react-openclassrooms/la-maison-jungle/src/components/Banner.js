45836ff9b593c0d3fdf925405653448e0e9a253b
LeLenoch <81lennoch@gmail.com>
2024-01-18 16:17:55 +0100

    Update

--- react-openclassrooms/la-maison-jungle/src/components/Banner.js
+++ react-openclassrooms/la-maison-jungle/src/components/Banner.js
@@ -1,24 +1,7 @@
 import '../styles/Banner.css'
-import logo from '../assets/leaf.png'
 
-function Banner() {
-    return (
-        <div 
-            style={{
-                color: 'white',
-                backgroundColor: '#31b572',
-                textAlign: 'right',
-                padding: '32px',
-                borderBottom: 'solid 3px black',
-                borderImage: ''
-            }}
-            className='lmj-banner'
-        >
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
-            <h1>La maison jungle</h1>
-            
-        </div>
-    )
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
--- react-openclassrooms/la-maison-jungle/src/components/ShoppingList.js
+++ react-openclassrooms/la-maison-jungle/src/components/ShoppingList.js
@@ -1,33 +1,34 @@
-const plantList = [
-    {
-        name: 'monstera',
-        category: 'classique',
-        id: '1ed',
-        isBestSale: true
-    },
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
+import '../styles/ShoppingList.css'
 
+function ShoppingList() {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-function ShoppingList({categories}) {
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={index}>
-                    {typeof plant === 'object' ? (
-                        <span>
-                            {plant.name} {plant.isBestSale ? '🔥' : '👎'}
-                        </span>
-                    ) : (
-                        <span>{plant}</span>
-                    )}
-                </li>
-            ))}
-        </ul>
-    )
+	return (
+		<div className='lmj-shopping-list'>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						key={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
