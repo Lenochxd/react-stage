6ca3fc18aa282cfcd8d4f3695f7cda2446cc4ffc
LeLenoch <81lennoch@gmail.com>
2024-01-30 16:35:29 +0100

    Moved Header.js

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Game from './components/Game';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import './styles/App.css';
 
 function App() {
