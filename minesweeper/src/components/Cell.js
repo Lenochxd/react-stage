d8aac85847aa7af08727c2948b67d169d81921c0
LeLenoch <81lennoch@gmail.com>
2024-02-02 15:33:19 +0100

    Added minewrong

--- minesweeper/src/components/Cell.js
+++ minesweeper/src/components/Cell.js
@@ -11,11 +11,10 @@ const renderSwitch = (value) => {
 };
 
 
-const Cell = ({ value, position, closed, flagged,
- onClick }) => {
+const Cell = ({ value, position, closed, flagged, minewrong, onClick }) => {
     return (
         <div
-            className={`cell type${value} ${closed} ${flagged}`}
+            className={`cell type${value} ${closed} ${flagged} ${minewrong}`}
             id={position}
             onClick={onClick}
             onContextMenu={onClick}
